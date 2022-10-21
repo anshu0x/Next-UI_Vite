10197d2dd0d6d9f48e5932654e91b3e262542451
anshu0x <anshusharma6327@gmail.com>
2022-10-21 21:43:20 +0530

    navbar created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarReact from "./components/NavbarReact";
+
+const App = () => {
+  return (
+    <>
+      <NavbarReact />
+    </>
+  );
+};
+
+export default App;
